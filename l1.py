18b42fe1496626bf441fd099a463686c501dd54c
Alexander Efimov <alexandr.efimov@gmail.com>
2016-01-30 00:40:47 +0500

    l1 = 5/6

---
+++ l1.py
@@ -0,0 +1,73 @@
+p = '/Users/malex/Dropbox/create/project/ML/l1/'
+
+
+def t1():
+    import numpy as np
+    X = np.random.normal(loc=1, scale=10, size=(1000, 50))
+    print(X)
+
+    m = np.mean(X, axis=0)
+    std = np.std(X, axis=0)
+    X_norm = ((X - m) / std)
+    print(X_norm)
+
+    Z = np.array([[4, 5, 0],
+                  [1, 9, 3],
+                  [5, 1, 1],
+                  [3, 3, 3],
+                  [9, 9, 9],
+                  [4, 7, 1]])
+    r = np.sum(Z, axis=1)
+    print(np.nonzero(r > 10))
+
+    A = np.eye(3)
+    B = np.eye(3)
+    print(A)
+    print(B)
+
+    AB = np.vstack((A, B))
+    print(AB)
+
+
+def pf(name, value):
+    f = open(name + '.txt', 'w')
+    f.write(str(value))
+    f.close()
+    print(name + '="%s"\n' % value)
+
+
+def main():
+    import pandas
+    data = pandas.read_csv(p + 'train.csv', index_col='PassengerId')
+
+    def q1():
+        d = data['Sex'].value_counts()
+        pf('1', '%s %s' % (d[0], d[1]))
+        print(d, '\n')
+
+    def q2():
+        d = data['Survived'].value_counts()
+        r = round(d[1] / d.sum() * 100, 2)
+        pf('2', r)
+        print(d, '\n')
+
+    def q3():
+        d = data['Pclass'].value_counts()
+        r = round(d[1] / d.sum() * 100, 2)
+        pf('3', r)
+        print(d, '\n')
+
+    def q4():
+        d = data['Age']
+        a = round(d.sum() / d.count(), 2)
+        m = d.median()
+        pf('4', '%s %s' % (a, m))
+
+    def q5():
+        d = round(data['SibSp'].corr(data['Parch']), 2)
+        pf('5', d)
+
+    q5()
+
+
+main()
